ac50c68031aa5446ef897bf1eb53dc0536d69540
sonnguyen0610 <17130194@st.hcmuaf.edu.vn>
2021-10-28 18:48:24 +0700

    - update

--- src/main/java/Cau2.java
+++ src/main/java/Cau2.java
@@ -2,15 +2,21 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cau2 {
     public static void main(String[] args) {
         String regex= "\\b[A-Za-z]+\\w*@([A-ZA-z]+\\.[A-Za-z]{2,})(\\\\.[A-Za-z]{2,})?\\b";
+        String path = "data.csv";
+        listSTHaveGmail(path);
     }
 
     //doc file
     public static void listSTHaveGmail(String path) {
         File file = new File(path);
+        Pattern p = Pattern.compile("\\b[a-zA-Z]\\w*(@{1})gmail(\\.[a-z]{2,}){1,2}");
+
         if (file.exists()) {
             try {
                 FileInputStream fis = new FileInputStream(path);
@@ -23,9 +29,12 @@ public class Cau2 {
                     int report = dis.readInt();
                     int app = dis.readInt();
                     double lt = dis.readDouble();
-                    if(email)
-                    Student outST = new Student(ID, name, email, bonus, report, app, lt);
-                    System.out.println(outST);
+                    Matcher m = p.matcher(email);
+                    if(m.matches()){
+
+                        Student outST = new Student(ID, name, email, bonus, report, app, lt);
+                        System.out.println(outST);
+                    }
                 }
                 dis.close();
                 fis.close();
